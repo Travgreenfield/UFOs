c58b4033737bb9e4698c76fbf0c012726cb1c107
Travgreenfield <travgreenfield@gmail.com>
2020-08-10 18:55:21 -0500

    Post filter button

--- static/js/app.js
+++ static/js/app.js
@@ -14,3 +14,17 @@ function buildTable(data) {
         });
     });
 }
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData);
+}
+
+// listen for and handle 'click'
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
